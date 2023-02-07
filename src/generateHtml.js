7f961b1ef3fac39d3bb856ba46f4b5187ab1e52c
Sofuto22 <zenigata19@gmail.com>
2023-02-07 17:04:04 -0500

    continue html.js

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -20,4 +20,24 @@ function generateCards(answers) {
       answers.icon = "fas fa-user-graduate";
       break;
   }
+  let output = `
+  <div class="card col-4">
+  <div class="card-body">
+    <h3 class="card-title">${answers.name}</h3>
+    <h5 class="card-text"><i class="${
+      answers.icon
+    } me-2"></i>${answers.getRole()}</h5>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">ID: ${answers.id}</li>
+    <li class="list-group-item">Email: <a href="mailto:${answers.email}">${
+  answers.email
+}</a></li>
+    <li class="list-group-item">${answers.wildcard}${
+  answers.wildcardInfo
+}</li>
+  </ul>
+</div>
+`;
+return output;
 }
