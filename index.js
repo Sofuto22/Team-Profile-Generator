e1f34cbbbd6f3630a420c8c863976ac621e115fb
Sofuto22 <zenigata19@gmail.com>
2023-02-09 16:40:46 -0500

    redid whole index

--- index.js
+++ index.js
@@ -1,234 +1,135 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const { type } = require("os");
-const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const generate = require("./src/generateHTML");
-const employeesArr = []; 
+const Employee = require("./lib/Employee");
 
-const mainMenu = async () => {
-    const answer = await inquirer.createPromptModule([
-        {
-            type: "list",
-            name: "menu",
-            message: "Options",
-            choices:["Add a team", "Exit"]
-        },
-    ]);
-    switch (answer.menu) {
-        case "Add a team":
-         return addManager();
-        case "Exit":
-            console.log("Goodbye");
-            break;
-    }
-};
-
-const addManager = async () => {
-    const { name, id, email, office} = await inquirer.createPromptModule([
-        {
-            type:"input",
-            name:"name",
-            message:"What is the manager's name?(Required)",
-            validate: validateHeaderName,
-        },
-        
-        {
-            type: "input",
-            name: "id",
-            message: "What is the manager's ID? (Required)",
-            validate: validateId,
-
-        },
-
-        {
-            type: "input",
-            name: "email",
-            message: "What is the manager's email? (Required)",
-            validate: validateEmail, 
-
-        },
-        
-        {
-            type: "input",
-            name: "office",
-            message: "What is the manager's office? (Required)",
-            validate: (officeInput) => {
-                if (officeInput) {
-                    return true;
-                } else {
-                    console.log("Please enter office.");
-                    return false;
-                }
-            },
-        },
-    ]);
-    const manager = new Manager(name, id, email, office);
-    employeesArr.push(manager);
-    return addUser();
-};
-
-const addUser = async () => {
-    const choices = await inquirer.prompt({
-      type: "list",
-      name: "role",
-      message: "Which employee are you adding? (Required)",
-      choices: ["Engineer", "Intern"],
-    });
-    switch (choices.role) {
-      default:
-      case "Engineer":
-        return addEngineer();
-      case "Intern":
-        return addIntern();
-    }
-  };
-  
-  function validateName(name) {
-    if (name) {
-      return true;
-    } else {
-      console.log("\n Please enter name!");
-      return false;
-    }
+function validateInfo() {
+  if (x!= "") {
+    return true;
+  } else {
+    alert ("Must enter the info required");
+    return false;
   }
-  
-  function validateId(id) {
-    if (id) {
-      if (employeesArr.map(({ id }) => id).includes(id)) {
-        console.log("\n That ID already exists!");
-        return false;
-      }
-      return true;
-    } else {
-      console.log("\n Please enter an ID!");
-      return false;
-    }
-  }
+}
 
-  const addEngineer = async () => {
-    const { name, id, email, github, addAnother } = await inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the engineer's name? (Required)",
-        validate: validateName,
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is the engineer's ID? (Required)",
-        validate: validateId,
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the engineer's email? (Required)",
-        validate: validateEmail,
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "What's your engineer's GitHub username?",
-        validate: (githubInput_1) => {
-          if (githubInput_1) {
-            return true;
-          } else {
-            console.log("Please enter the engineer's GitHub username!");
-            return false;
-          }
-        },
-      },
-      {
-        type: "confirm",
-        name: "addAnother",
-        message: "Do you want to add another employee?",
-        default: "true",
-      },
-    ]);
-    employeesArr.push(new Engineer(name, id, email, github));
-    if (addAnother) {
-      return addUser();
-    }
-    return employeesArr;
-  };
+inquirer.prompt( [{
+  type:"input",
+  name:"name",
+  message: "Name of manager?",
+  validate: validateInfo,
+},
 
-  const addIntern = async () => {
-    const { name, id, email, school, addAnother } = await inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the intern's name? (Required)",
-        validate: validateName,
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is the intern's ID? (Required)",
-        validate: validateId,
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the intern's email? (Required)",
-        validate: validateEmail,
-      },
-      {
-        type: "input",
-        name: "school",
-        message: "What's your intern's school?",
-        validate: (schoolInput_1) => {
-          if (schoolInput_1) {
-            return true;
-          } else {
-            console.log("Please enter the intern's school!");
-            return false;
-          }
-        },
-      },
-      {
-        type: "confirm",
-        name: "addAnother",
-        message: "Do you want to add another employee?",
-        default: "true",
-      },
-    ]);
-    employeesArr.push(new Intern(name, id, email, school));
-    if (addAnother) {
-      return addUser();
-    }
-    return employeesArr;
-  };
-   
-  const writeToFile = (data) => {
-     console.log(data);
-    return new Promise((resolve, reject) => {
-      fs.writeFile("./dist/index.html", data, (err) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve({
-          ok: true,
-          message: "HTML page created!",
-        });
-      });
-    });
-  };
+{
+  type:"input",
+  name:"office",
+  message:"What is the office number for the manager?",
+  validate: validateInfo,
+},
+
+{
+  type:"input",
+  name:"email",
+  message:"What is the manager's email?",
+  validate: validateInfo,
+},
+
+{
+  type:"input",
+  name: "ID",
+  message: "What is the manager's ID number?",
+  validate: validateInfo,
+},
+{
+    type:"input",
+    names:"name",
+    message: "What is your name?",
+    validate:validateInfo,
+},
+{
+    type:"input",
+    name:"ID",
+    message:"What is the engineer's ID?",
+    validate:validateInfo,
+},
+{
+    type:"input",
+    name:"school",
+    message:"What school did the engineer attend?",
+    validate:validateInfo,
+},
   
-  function init() {
-    mainMenu()
-      .then((response) => {
-         console.log(response);
-         console.log(employeesArr);
-        return generate.generatePage(response);
-      })
-      .then((res) => {
-        writeToFile(res);
-        console.log("Success! Check out your generated HTML page!");
-      });
-  }
+{
+    type:"input",
+    name:"email",
+    message:"What is the engineer's email?",
+    validate:validateInfo,
+},
+
+{
+    type:"input",
+    name:"Github",
+    message:"What is the Github username?",
+    validate:validateInfo,
+},
+
+
+
+{
+    type:"input",
+    name:"name",
+    message:"What is the employee's name?",
+    validate:validateInfo
+},
+{
+    type:"input",
+    name:"ID",
+    message:"What is employee ID?",
+    validate:validateInfo,
+},
+{
+    type:"input",
+    name:"email",
+    message:"What is employee's email?",
+    validate:validateInfo,
+ },
+
+
   
-  init();
+  {
+    type:"input",
+    name:"name",
+    message:"What is the intern's name?",
+    validate: validateInfo,
+
+  },
+
+  {
+    type:"input",
+    name:"ID",
+    message:"What is intern's ID?",
+    validate:validateInfo,
+  },
+
+  {
+    type:"input",
+    type:"email",
+    message:"What is the email address of the intern?",
+    validate:validateInfo,
+  },
+
+  {
+    type:"input",
+    name:"school",
+    message:"Which school does the intern attend?",
+  },
+])
+
+.then(answers => {
+  console.info('Answers:', answers);
+});
+
+
+
+
